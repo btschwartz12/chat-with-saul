e701fa309144c8451804321e4bf448e037213e4c
Ben Schwartz <btschwartz12@gmail.com>
2023-07-27 00:09:02 -0400

    Fixed gui

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -1,25 +1,19 @@
 import { useState, useEffect } from "react";
-import { usePostLoginMutation, usePostSignUpMutation, usePostTokenLoginMutation } from "@/state/api";
+import { usePostTokenLoginMutation } from "@/state/api";
+import { Spinner } from "react-bootstrap";
+
+// import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Login = ({ setUser, setSecret }) => {
-  const [isRegister, setIsRegister] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  const [triggerLogin, resultLogin] = usePostLoginMutation();
-  const [triggerSignUp, resultSignUp] = usePostSignUpMutation();
+  const [isLoading, setIsLoading] = useState(false); // New state to control the spinner
   const [triggerToken, resultToken] = usePostTokenLoginMutation();
 
-  const handleLogin = () => {
-    triggerLogin({ username, password });
-  };
-
-  const handleRegister = () => {
-    triggerSignUp({ username, password });
-  };
 
   const handleToken = () => {
+    setIsLoading(true); // Starts loading when login is initiated
     triggerToken({ token });
   };
 
@@ -28,57 +22,23 @@ const Login = ({ setUser, setSecret }) => {
       const { username, secret, avatar, response } = resultToken.data;
       setUser(username);
       setSecret(secret);
+      setIsLoading(false); // Stops loading when data is fetched
     } else if (resultToken.error) {
       setErrorMessage("Token failed. Please check your credentials.");
+      setIsLoading(false); // Stops loading when error occurs
     }
   }, [resultToken.data, resultToken.error]);
-
-
-  useEffect(() => {
-    if (resultLogin.data?.response) {
-      setUser(username);
-      setSecret(password);
-    } else if (resultLogin.error) {
-      setErrorMessage("Login failed. Please check your credentials.");
-    }
-  }, [resultLogin.data, resultLogin.error]);
-
-  useEffect(() => {
-    if (resultSignUp.error) {
-      setErrorMessage("Already registered. Please login.");
-    }
-  }, [resultSignUp.error]);
 
   return (
     <div className="login-page">
       <div className="login-container">
-        <h2 className="title">CHAT WITH GPT</h2>
-        <p
-          className="register-change"
-          onClick={() => setIsRegister(!isRegister)}
-        >
-          {isRegister ? "Already a user?" : "Are you a new user?"}
-        </p>
+        <h2 className="title">BLISS CHAT</h2>
 
         <div>
           <input
             className="login-input"
             type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            className="login-input"
-            type="text"
-            placeholder="Token"
+            placeholder="Enter Token..."
             value={token}
             onChange={(e) => setToken(e.target.value)}
           />
@@ -87,23 +47,18 @@ const Login = ({ setUser, setSecret }) => {
         {errorMessage && <p className="error-message">{errorMessage}</p>}
 
         <div className="login-actions">
-          {isRegister ? (
-            <button type="button" onClick={handleRegister}>
-              Register
-            </button>
-          ) : (
-            <button type="button" onClick={handleLogin}>
-              Login
-            </button>
-          )}
-        </div>
-
-        <div className="login-actions">
-
-            <button type="button" onClick={handleToken}>
-              Token
-            </button>
- 
+          <a className="btn btn-info" target="_blank" href="https://btschwartz.com/api/v1/auth/login">
+            Get Token
+          </a>
+          <button className="btn btn-primary" style={{marginLeft: "10px"}} type="button" onClick={handleToken}>
+            {isLoading ? (
+              <Spinner animation="border" role="status" size="sm">
+                
+              </Spinner>
+            ) : (
+              'Login'
+            )}
+          </button>
         </div>
       </div>
     </div>
